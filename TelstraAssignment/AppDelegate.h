bc12603df5026b47807bcf7935909fb62c3baea7
Swapnil <swapniltambe1111@gmail.com>
2016-12-20 11:32:26 +0530

    Initial Commit
    Creating Initial project

---
+++ TelstraAssignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TelstraAssignment
+//
+//  Created by mac_admin on 20/12/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TelstraAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TelstraAssignment
+//
+//  Created by mac_admin on 20/12/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
